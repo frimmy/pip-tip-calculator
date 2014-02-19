646882cabe5bf92b3c318eda54cfab45afc2913d
adrian.frimpong <adrian.frimpong@gmail.com>
2014-02-19 17:43:37 -0500

    completed ver2 of tip calculator

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,6 +1,6 @@
-meal = 20
-tax = .12
-tip = .20 
+meal = float(raw_input("How much did your meal cost? >: "))
+tax = float(raw_input("What's the tax rate? >: "))
+tip = float(raw_input("What percentage are you going to tip? >: ")) 
 
 tax_value = meal*tax
 meal_with_tax = meal + tax_value
@@ -9,6 +9,6 @@ tip_value = meal_with_tax*tip  #raw val of tip given meal_with_tax
 total = tip_value + meal_with_tax #cost of meal_with_tax + tip_value
 
 print "Base cost of meal: ${:.2f}".format(meal)  
-print "Dollar value of tax on the meal: ${:.2f}".format(tax_value)
-print "Dollar value given of tip(given tip %): ${:.2f}".format(tip_value)
+print "Dollar value of tax on the meal based on a tax rate of {:.2%}: ${:.2f}".format(tax, tax_value)
+print "Dollar value given you tip {:.2%}: ${:.2f}".format(tip, tip_value)
 print "Grand total for the meal(including tax & tip): ${:.2f}".format(total)
